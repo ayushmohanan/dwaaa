41f15770e6f04d71e08ca12fce4e016aec9590f7
LAPTOP-5GIKFF4H\viewy <ayush@viewydigital.com>
2023-02-10 14:42:37 +0530

    First Commit

--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -1,8 +1,9 @@
 <div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
     <div class="sidebar-brand d-none d-md-flex">
         <a href="{{route("backend.dashboard")}}">
-            <img class="sidebar-brand-full" src="{{asset("img/backend-logo.jpg")}}" height="46" alt="{{ app_name() }}">
-            <img class="sidebar-brand-narrow" src="{{asset("img/backend-logo-square.jpg")}}" height="46" alt="{{ app_name() }}">
+            <img class="sidebar-brand-full" src="{{asset("img/backend-logo.png")}}" height="46" alt="{{ app_name() }}">
+            <img class="sidebar-brand-narrow" src="{{asset("img/backend-logo-square.png")}}" height="46"
+                alt="{{ app_name() }}">
         </a>
         <!-- <svg class="sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
             <use xlink:href="assets/brand/coreui.svg#full"></use>
@@ -12,7 +13,8 @@
         </svg> -->
     </div>
 
-    {!! $admin_sidebar->asUl( ['class' => 'sidebar-nav', 'data-coreui'=>'navigation', 'data-simplebar'], ['class' => 'nav-group-items'] ) !!}
+    {!! $admin_sidebar->asUl( ['class' => 'sidebar-nav', 'data-coreui'=>'navigation', 'data-simplebar'], ['class' =>
+    'nav-group-items'] ) !!}
 
     <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
 </div>
